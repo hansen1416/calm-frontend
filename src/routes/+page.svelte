c85eda6d1c0d562ccd2ce5588c5d501a7c0a0224
hansen1416 <L.Han2@uni.brighton.ac.uk>
2026-01-22 17:44:41 +0000

    1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { onMount } from "svelte";
+	import { apiFetch } from "$lib/api";
 
 	let data = null;
 
 	onMount(async () => {
 		// Note: Do NOT use port 5173 here. Use localhost (port 80).
-		const response = await fetch("http://localhost/api/ping");
+		const response = await apiFetch("api/ping");
 		data = await response.json();
 
 		console.log(data);
