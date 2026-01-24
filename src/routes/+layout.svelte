377421591882470a37e28cf754ce535be5532975
hansen1416 <L.Han2@uni.brighton.ac.uk>
2026-01-24 16:06:04 +0000

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,16 +18,6 @@
 	<header class="h-14 border-b flex items-center px-4">
 		<div class="font-semibold">
 			<a href="/" class="font-semibold">Calm</a>
-
-			<Button onclick={toggleMode} variant="outline" size="icon">
-				<SunIcon
-					class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
-				/>
-				<MoonIcon
-					class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
-				/>
-				<span class="sr-only">Toggle theme</span>
-			</Button>
 		</div>
 
 		<div class="ml-auto flex items-center gap-2">
@@ -55,6 +45,24 @@
 					</NavigationMenu.Item>
 				</NavigationMenu.List>
 			</NavigationMenu.Root>
+
+			<!-- right-most theme toggle -->
+			<div class="pl-2">
+				<Button
+					class="relative"
+					onclick={toggleMode}
+					variant="outline"
+					size="icon"
+				>
+					<SunIcon
+						class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+					/>
+					<MoonIcon
+						class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</div>
 		</div>
 	</header>
 
